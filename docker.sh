750d77aa269d343b9ced5e33760b4f1e7b260a46
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-09-25 19:13:08 +0530

    docker

--- docker.sh
+++ docker.sh
@@ -3,13 +3,13 @@
 echo "::::INSTALLING DOCKER::::"
 sudo yum check-update
 curl -fsSL https://get.docker.com/ | sh
-systemctl enable docker && systemctl restart docker && systemctl status docker
+systemctl enable docker && systemctl restart docker && systemctl status docker >> /tmp/dockerInstallation.logs
 
 echo "::::INSTALLING DOCKER COMPOSE::::"
 VERSION=$(curl -s https://github.com/docker/compose/tags | grep tar.gz | grep -v '\-rc' | grep nofollow | head -1 |awk -F / '{print $NF}' | awk '{print $1}' | sed -e 's/.tar.gz"//')
 curl -s -L "https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
-chmod +x /bin/docker-compose
-docker-compose --version &>/dev/null 
+chmod +x /bin/docker-compose /tmp/dockerCompose.ver
+docker-compose --version
 if [ $? -eq 127 -o $? -eq 126 ]; then 
   echo -e "\e[31m Installation Failed"
   exit 1
