0eb51e5bd3a4a209535d12fb9b0959ccf02d83f0
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-09-25 18:37:12 +0530

    added extras

---
+++ script.sh
@@ -0,0 +1,41 @@
+sudo vim /etc/ssh/sshd_config
+PermitRootLogin yes
+PasswordAuthentication yes
+sudo systemctl restart sshd
+
+yum update -y
+
+sudo yum -y install epel-release
+sudo yum git install vim zip jq cockpit wget make cmake gcc bzip2-devel libffi-devel zlib-devel openssl-devel
+sudo yum -y groupinstall "Development Tools"
+
+yum update -y
+
+yum install -y python3.11 python3.11-pip
+
+pip3 install awscli boto3 ansible 
+
+
+
+## DOCKER INSTALALTION ##
+sudo yum check-update
+curl -fsSL https://get.docker.com/ | sh
+systemctl enable docker && systemctl restart docker && systemctl status docker
+## DOCKER COMPOSED INSTALALTION ##
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+## INSTALL JENKINS ##
+# Add required dependencies for the jenkins package
+yum install fontconfig java-11-openjdk-devel wget -y
+wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins --nogpgcheck -y
+systemctl enable jenkins && systemctl restart jenkins
+mkdir -p /var/lib/jenkins/.ssh
+echo 'Host *
+    UserKnownHostsFile /dev/null
+    StrictHostKeyChecking no' >/var/lib/jenkins/.ssh/config
+chown jenkins:jenkins /var/lib/jenkins/.ssh -R
+chmod 400 /var/lib/jenkins/.ssh/config
