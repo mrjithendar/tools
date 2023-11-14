b1eff48c6e56d328fd3f85119e8ac3d03b42199c
jithendar <jithendardharmapuri@gmail.com>
2023-11-14 20:14:53 +0530

    added vault

--- hashiCorp.sh
+++ hashiCorp.sh
@@ -3,10 +3,10 @@ echo "installing Terraform latest"
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
-echo "installed terraform $(terraform -version)"
-
 
 ## VAULT INSTALALTION ##
 echo "installing Terraform vault latest"
 sudo yum -y install vault
+
+echo "installed terraform $(terraform -version)"
 echo "installed terraform vault $(vault -version)"
