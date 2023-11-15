18121b3a2513c2e6a9636cb9927803957ff3a916
jithendar <jithendardharmapuri@gmail.com>
2023-11-15 11:43:46 +0530

    fix vault only installation with UI enabled

---
+++ terraform.sh
@@ -0,0 +1,9 @@
+## TERRAFORM INSTALALTION ##
+echo "installing Terraform latest"
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+
+echo "installed terraform $(terraform -version)"
+
+
