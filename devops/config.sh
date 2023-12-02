08771996fa52e5edb848112777712d806a093422
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-02 11:54:03 +0530

    python

--- devops/config.sh
+++ devops/config.sh
@@ -1,11 +1,3 @@
-yum update -y
-
-yum upgrade -y
-
-yum install python3.11 python3.11 pip -y
-
-ln -s /usr/bin/pip3.11 /usr/bin/python
-ln -s /usr/bin/pip3.11 /usr/bin/pip
-
-pip install checkov
-pip install ansible
+echo -e "installing python"
+yum install python36 python3 pip -y
+echo -e "installed python version $(python --version)"
