5252302d4c33fc369580fdd87d70a0bc3a248420
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-02 11:40:06 +0530

    test

--- devops/packer.sh
+++ devops/packer.sh
@@ -1,4 +1,4 @@
 echo "installing Packer latest"
-curl -L wget https://releases.hashicorp.com/packer/1.9.4/packer_1.9.4_linux_amd64.zip -o /tmp/packer.zip
+curl -L https://releases.hashicorp.com/packer/1.9.4/packer_1.9.4_linux_amd64.zip -o /tmp/packer.zip
 unzip /tmp/packer.zip -d /usr/sbin
 echo "installed packer Successfully"
