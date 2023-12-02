528bb5955b0095943ebc66ab6368f22ac13318e8
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-02 21:02:13 +0530

    added loki

---
+++ devops/loki.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -L https://github.com/grafana/loki/releases/download/v2.8.6/loki-linux-amd64.zip -o /tmp/loki.zip
+unzip /tmp/loki.zip -d /tmp
+cp /tmp/loki-linux-amd64 /usr/bin/loki
+chmod a+x /opt/loki/loki
+
+cp /tmp/tools/devops/dependencies/loki.yml /opt/loki/loki.yml
+cp /tmp/tools/devops/dependencies/loki.service /etc/systemd/system/loki.service
+
+systemctl daemon-reload
+systemctl enable loki
+systemctl start loki
+
+echo -e "installed loki successfully"
