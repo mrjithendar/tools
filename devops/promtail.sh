79cc21eb7d56be90e1f4c6d8f94ac0456e68badb
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-02 21:21:33 +0530

    added promtail

---
+++ devops/promtail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -L https://github.com/grafana/loki/releases/download/v2.8.6/promtail-linux-amd64.zip-o /tmp/promtail.zip
+unzip /tmp/promtail.zip -d /tmp
+
+cp /tmp/promtail-linux-amd64 /opt/loki/promtail
+
+cp /tmp/tools/devops/dependencies/promtail.yml /opt/loki/promtail.yml
+
+systemctl daemon-reload
+systemctl enable promtail
+systemctl start promtail
+
+echo -e "installed promtail"
