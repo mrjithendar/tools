195178b0821830710b6b5a0ae99ea4fcc810c766
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-02 21:22:03 +0530

    added promtail

--- devops/promtail.sh
+++ devops/promtail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/grafana/loki/releases/download/v2.8.6/promtail-linux-amd64.zip-o /tmp/promtail.zip
+curl -L https://github.com/grafana/loki/releases/download/v2.8.6/promtail-linux-amd64.zip -o /tmp/promtail.zip
 unzip /tmp/promtail.zip -d /tmp
 
 cp /tmp/promtail-linux-amd64 /opt/loki/promtail
