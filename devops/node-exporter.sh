04481a2ffed81597f0e23df8893bb2f7a22beb3a
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-02 18:10:32 +0530

    ne

--- devops/node-exporter.sh
+++ devops/node-exporter.sh
@@ -5,12 +5,6 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi
 
-if [ -d /opt/node_exporter ]; then
-  systemctl enable node_exporter
-  systemctl start node_exporter
-  exit 0
-fi
-
 URL=$(curl -L -s https://prometheus.io/download/  | grep tar | grep node_exporter | grep linux-amd64  | sed -e "s|>| |g" -e 's|<| |g' -e 's|"| |g' |xargs -n1 | grep ^http | head -1)
 
 FILENAME=$(echo $URL | awk -F / '{print $NF}')
@@ -20,7 +14,11 @@ curl -L $URL -o /tmp/node_exporter.tar.gz
 tar -xf /tmp/node_exporter.tar.gz -C /tmp
 mv /tmp/$DIRNAME /tmp/node_exporter
 
-cp dependencies/node_exporter.service /etc/systemd/system/node_exporter.service
-systemctl enable node_exporter
-systemctl start node_exporter
+if [ -d /opt/node_exporter ]; then
+  cp dependencies/node_exporter.service /etc/systemd/system/node_exporter.service
+  systemctl enable node_exporter
+  systemctl start node_exporter
+  exit 0
+fi
+
 echo -e "node_exporter Installed successfully"
