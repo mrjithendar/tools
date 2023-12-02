2d853ef4cb7d6f01fe44f8ccf84d659290fa8acd
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-02 11:52:21 +0530

    aws

--- devops/aws_cli.sh
+++ devops/aws_cli.sh
@@ -1,6 +1,6 @@
 sudo yum remove awscli
 echo -e "installing latest AWS CLI"
 curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o /tmp/awscliv2.zip
-unzip /tmp/awscliv2.zip /tmp/
+unzip -o /tmp/awscliv2.zip -d /tmp
 sh /tmp/aws/install
 echo "Installed AWS CLI Version $(aws --version)"
