39dd07003cb9ec8f1cb322abe19d8b561275fb96
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-08 09:56:57 +0530

    added trivy docker image scanner

--- devops/trivy.sh
+++ devops/trivy.sh
@@ -6,3 +6,5 @@ enabled=1' > /etc/yum.repos.d/trivy.repo
 
 #sudo yum -y update
 sudo yum -y install trivy
+echo -e "successfully installed $(trivy --version)"
+echo -e "for more details please visit:: https://aquasecurity.github.io/trivy/v0.18.3/installation/"
