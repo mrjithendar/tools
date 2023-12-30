8c79c92b57b6855188cff8b90bd93c1c6c4d926a
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-30 13:27:47 +0530

    vault

--- devops/vault.sh
+++ devops/vault.sh
@@ -18,7 +18,7 @@ systemctl start vault
 
 echo "installed terraform vault $(vault -version)"
 
-vault policy write jenkins devops/dependencies/vault/jenkins-policy.hcl
+vault policy write jenkins devops/dependencies/vault/jenkins_policy.hcl
 
 # export VAULT_ADDR='http://127.0.0.1:8200'
 # vault status # Get the vault status
