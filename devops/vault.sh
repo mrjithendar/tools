896b92f8cbde7614bbe5e3a3d589bd790fe6a87b
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-12-30 18:35:17 +0530

    vault

--- devops/vault.sh
+++ devops/vault.sh
@@ -10,7 +10,7 @@ mkdir -p /etc/vault/data
 mkdir -p /var/lib/vault/
 pwd
 rm -rf /etc/vault.d/vault.hcl
-cp devops/dependencies/vault/vault.hcl /etc/vault.d/vault.hcl
+cp devops/dependencies/vault/vault.hcl /var/lib/vault/vault.hcl
 cp devops/dependencies/vault/vault.service /etc/systemd/system/vault.service
 
 systemctl daemon-reload
