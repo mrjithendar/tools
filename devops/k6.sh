4fbb85e04d7b23a854fa0d9772601897e2c20d7e
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-16 12:00:45 +0530

    added k6

--- devops/k6.sh
+++ devops/k6.sh
@@ -7,14 +7,15 @@ if [ "$OS" == "CentOS 8" ]; then
     echo "OS is valid, currently running on: $OS"
 else
     echo "Please use CentOS AMI, Currently Running on: $OS"
+    exit 1
 fi
 
-# if [ $(id -u) -ne 0 ]; then
-#   echo "You should run as root user"
-#   exit 1
-# fi
+if [ $(id -u) -ne 0 ]; then
+  echo "You should run as root user"
+  exit 1
+fi
 
-# dnf install https://dl.k6.io/rpm/repo.rpm -y
-# dnf install k6 -y
+dnf install https://dl.k6.io/rpm/repo.rpm -y
+dnf install k6 -y
 
-# echo "visit https://k6.io/docs/get-started/installation/ for more details"
+echo "visit https://k6.io/docs/get-started/installation/ for more details"
