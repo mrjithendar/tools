4d60b720a3140de5d3577f62f6c0146a26353bb2
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-16 11:54:05 +0530

    added k6

--- devops/k6.sh
+++ devops/k6.sh
@@ -9,12 +9,12 @@ if [ OS == "CentOS 88" ]; then
         echo "Please use CentOS AMI"
 fi
 
-if [ $(id -u) -ne 0 ]; then
-  echo "You should run as root user"
-  exit 1
-fi
+# if [ $(id -u) -ne 0 ]; then
+#   echo "You should run as root user"
+#   exit 1
+# fi
 
-dnf install https://dl.k6.io/rpm/repo.rpm
-dnf install k6
+# dnf install https://dl.k6.io/rpm/repo.rpm -y
+# dnf install k6 -y
 
-echo "visit https://k6.io/docs/get-started/installation/ for more details"
+# echo "visit https://k6.io/docs/get-started/installation/ for more details"
