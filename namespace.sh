77c77203ca656244b5e42f99d3293d72d4a7d5c4
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-17 15:58:56 +0530

    generic commit

--- namespace.sh
+++ namespace.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+kubectl get ns | grep roboshop
+
+if [ $? -eq 0 ]; then
+    echo "namespace existing, continue to deploy microcsevice."
+else
+    kubectl create namespace roboshop
+    echo "roboshop namesapce is created, contunue deploying microservice."
+fi
