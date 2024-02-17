e4305456c150efdf82ebdc95be8dddacd1ed29c9
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-17 11:08:42 +0530

    generic commit

---
+++ devops/gitlab-runner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## check OS
+OS=$(cat /etc/redhat-release | awk {'print $1, $NF'})
+
+if [ "$OS" == "CentOS 8" ]; then
+    echo "OS is valid, currently running on: $OS"
+else
+    echo "Please use CentOS AMI, Currently Running on: $OS"
+    exit 1
+fi
+
+## user check
+if [ $(id -u) -ne 0 ]; then
+  echo "You should run as root user"
+  exit 1
+fi
+
+if [ -d /opt/alertmanager ]; then
+  exit 0
+fi
+
+uname -a 
+
+# Download the binary for your system
+curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+# Give it permission to execute
+chmod +x /usr/local/bin/gitlab-runner
+
+# Create a GitLab Runner user
+useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+
+# Install and run as a service
+gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+gitlab-runner start
